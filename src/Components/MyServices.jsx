e156400ae0186ad8da8e2130c40d8fd1ad1179a3
mhdsafar <muhammedsafar9567.com>
2026-03-26 12:18:05 +0530

    update

--- src/Components/MyServices.jsx
+++ src/Components/MyServices.jsx
@@ -28,11 +28,11 @@ const MyServices = () => {
   }
 ];
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col gap-5 px-4 md:px-0">
       <div className="flex">
         <h1 className='text-3xl font-bold '>
         My Services</h1></div>
-      <div className="grid grid-cols-3 justify-between">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
        {services.map((service, i) => (
   <div
     key={i}
